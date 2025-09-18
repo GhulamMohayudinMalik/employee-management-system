a7469a6fdd9ece74b0d840f018d5ddb896da7286
Ghulam Mohayudin <ghulammohayudinmalik@gmail.com>
2025-09-18 19:14:24 +0500

    Update admin dashboard to show employees stats

--- src/components/Other/AllTask.jsx
+++ src/components/Other/AllTask.jsx
@@ -1,37 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 function AllTask() {
+
+  const authData = useContext(AuthContext)
+
   return (
-    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-52 overflow-auto'>
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Ghulam Mohayudin</h2>
-        <h3>Make A UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Ghulam Mohayudin</h2>
-        <h3>Make A UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Ghulam Mohayudin</h2>
-        <h3>Make A UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Ghulam Mohayudin</h2>
-        <h3>Make A UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className='bg-orange-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Ghulam Mohayudin</h2>
-        <h3>Make A UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Ghulam Mohayudin</h2>
-        <h3>Make A UI design</h3>
-        <h5>Status</h5>
+          <h2 className='w-1/5 bg-red-600'>Employee Name</h2>
+          <h3 className='w-1/5 bg-red-600'>New Task</h3>
+          <h5 className='w-1/5 bg-red-600'>Active Task</h5>
+          <h5 className='w-1/5 bg-red-600'>Completed Task</h5>
+          <h5 className='w-1/5 bg-red-600'>Failed Task</h5>
+        </div>
+      <div>
+        {authData.employees.map((e, id) => {
+        return (
+        <div key={id} className='border-emerald-500 border-2 mb-2 py-2 px-4 flex justify-between rounded'>
+          <h2 className='w-1/5'>{e.firstname}</h2>
+          <h3 className='w-1/5 text-lg font-medium !text-blue-400'>{e.taskCount.newTask}</h3>
+          <h5 className='w-1/5 text-lg font-medium !text-yellow-600'>{e.taskCount.active}</h5>
+          <h5 className='w-1/5 text-lg font-medium'>{e.taskCount.completed}</h5>
+          <h5 className='w-1/5 text-lg font-medium !text-red-600'>{e.taskCount.failed}</h5>
+        </div>
+        )
+      })}
       </div>
     </div>
   )
